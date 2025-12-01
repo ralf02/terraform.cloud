e22ce3db7a072d918d0f4553cc160e9cbe04c195
Rafiko <ralfcontreras@gmail.com>
2025-12-01 11:16:51 -0500

    :bug: Solucionando error Error: Reference to "each" in context without for_each

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,4 @@
 output "ec2_public_ip" {
     description = "Ip publica de la instacia web01"
-
-    #value = aws_instance.web01.public_ip
-
-    #cuanto es con for_each
-    value = aws_instance.web01[each.key]
+    value = aws_instance.web01.public_ip
 }
