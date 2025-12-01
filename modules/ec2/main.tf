d1167773b8340c0516a31d0e8a94f0f6ee756910
Rafiko <ralfcontreras@gmail.com>
2025-12-01 14:47:10 -0500

    :boom: Terraform Cloud

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -15,29 +15,29 @@ resource "aws_instance" "MyEC2" {
         delete_on_termination = true
     }
 
-    provisioner "local-exec" {
-        command = <<EOT
-            if [ -f "../../../../.env" ]; then
-            if grep -q '^ANSIBLE_HOST=' "../../../../.env"; then
-                # Update existing ANSIBLE_HOST
-                sed -i "s|^ANSIBLE_HOST=.*|ANSIBLE_HOST=${self.public_ip}|" "../../../../.env"
-            else
-                # Add ANSIBLE_HOST if it doesn't exist
-                echo "ANSIBLE_HOST=${self.public_ip}" >> "../../../../.env"
-            fi
-            else
-            # Create .env file with ANSIBLE_HOST
-            echo "ANSIBLE_HOST=${self.public_ip}" > "../../../../.env"
-            fi
-        EOT
-    }
-
-    provisioner "local-exec" {
-        command =  "echo 'MyEC2 IP ${self.public_ip} creada' >> ../../../logs/logs.log"
-    }
-
-    provisioner "local-exec" {
-        when = destroy
-        command =  "echo 'MyEC2 IP ${self.public_ip} destruida' >> ../../../logs/logs.log"
-    }
+    #     provisioner "local-exec" {
+    #         command = <<EOT
+    #             if [ -f "../../../../.env" ]; then
+    #             if grep -q '^ANSIBLE_HOST=' "../../../../.env"; then
+    #                 # Update existing ANSIBLE_HOST
+    #                 sed -i "s|^ANSIBLE_HOST=.*|ANSIBLE_HOST=${self.public_ip}|" "../../../../.env"
+    #             else
+    #                 # Add ANSIBLE_HOST if it doesn't exist
+    #                 echo "ANSIBLE_HOST=${self.public_ip}" >> "../../../../.env"
+    #             fi
+    #             else
+    #             # Create .env file with ANSIBLE_HOST
+    #             echo "ANSIBLE_HOST=${self.public_ip}" > "../../../../.env"
+    #             fi
+    #         EOT
+    #     }
+    #
+    #     provisioner "local-exec" {
+    #         command =  "echo 'MyEC2 IP ${self.public_ip} creada' >> ../../../logs/logs.log"
+    #     }
+    #
+    #     provisioner "local-exec" {
+    #         when = destroy
+    #         command =  "echo 'MyEC2 IP ${self.public_ip} destruida' >> ../../../logs/logs.log"
+    #     }
 }
